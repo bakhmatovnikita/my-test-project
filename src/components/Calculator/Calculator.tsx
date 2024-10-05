aff31d9fb4466429c55d288a850c75308c125c8c
bahmatov <bahmatov@liliani.ru>
2024-10-05 14:07:21 +0300

    a little visual changes

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,133 +1,119 @@
-import React, { useEffect, useState } from "react";
-
-const add = (a: number, b: number): number => a + b;
-const subtract = (a: number, b: number): number => a - b;
-const multiply = (a: number, b: number): number => a * b;
-const divide = (a: number, b: number): number => b !== 0 ? a / b : Infinity;
+import { Space } from "antd";
+import React, { useState } from "react";
 
 export const Calculator = () => {
-  const [display, setDisplay] = useState('');
-  const [result, setResult] = useState('');
+  const [num, setNum] = useState(0);
+  const [oldNum, setOldNum] = useState(0);
+  const [operator, setOperator] = useState();
 
-  const handleInput = (value: string | number) => {
-    if (typeof value === 'number' && !isNaN(value)) {
-      setDisplay(prev => prev + value.toString());
-    } else if (['+', '-', '*', '/', '%'].includes(value)) {
-      if (!display.includes('=')) {
-        setDisplay(prev => prev + value);
-      }
-    } else if (value === 'AC') {
-      clearDisplay();
-    } else if (value === '=') {
-      calculateResult();
-    } else if (value === '+/-') {
-      setDisplay(prev => prev === '-' ? '' : '-');
+  const inputNum = (e: any) => {
+    let input = e.target.value;
+    if (num === 0) {
+      setNum(input);
     } else {
-      setDisplay(prev => prev + value);
+      setNum(num + input);
     }
   };
 
-  const calculateResult = () => {
-    let result = eval(display.replace(/[^0-9+-.]/g, '')).toString();
-    
-    const operations = display.match(/[+\-*/%]/g) || [];
-    for (let i = 0; i < operations.length; i++) {
-      switch (operations[i]) {
-        case '+':
-          result = add(parseFloat(result), parseFloat(display.split(operations[i])[1]));
-          break;
-        case '-':
-          result = subtract(parseFloat(result), parseFloat(display.split(operations[i])[1]));
-          break;
-        case '*':
-          result = multiply(parseFloat(result), parseFloat(display.split(operations[i])[1]));
-          break;
-        case '/':
-          result = divide(parseFloat(result), parseFloat(display.split(operations[i])[1]));
-          break;
-        case '%':
-          result = multiply(parseFloat(result), 0.01);
-          break;
-      }
-    }
-
-    setResult(result);
-    setDisplay(result);
+  const clear = () => {
+    setNum(0);
   };
 
-  const applyOperation = (operation: string) => {
-    try {
-      const newCalculation = eval(`${result}${operation}`);
-      setResult(newCalculation.toString());
-      setDisplay(result);
-    } catch (error) {
-      setResult('Ошибка');
-      setDisplay(result);
+  const porcentagem = (e: any) => {
+    setNum(num / 100);
+  };
+
+  const changeSign = () => {
+    if (num > 0) {
+      setNum(-num);
+    } else {
+      setNum(Math.abs(num));
     }
   };
 
-  const clearDisplay = () => {
-    setDisplay('');
-    setResult('');
+  const operatorHandler = (e: any) => {
+    let operatorInput = e.target.value;
+    setOperator(operatorInput);
+    setOldNum(num);
+    setNum(0);
   };
 
-  useEffect(() => {
-    if (display !== '') {
-      try {
-        const lastAction = display.slice(-1)[0];
-        switch (lastAction) {
-          case '+':
-            applyOperation('+');
-            break;
-          case '-':
-            applyOperation('-');
-            break;
-          case '*':
-            applyOperation('*');
-            break;
-          case '/':
-            applyOperation('/');
-            break;
-          default:
-            break;
-        }
-      } catch (error) {
-        console.error('Ошибка при выполнении последнего действия:', error);
-      }
+  const calculate = () => {
+    if (operator === "/") {
+      setNum(parseFloat(oldNum) / parseFloat(num));
+    } else if (operator === "X") {
+      setNum(parseFloat(oldNum) * parseFloat(num));
+    } else if (operator === "-") {
+      setNum(parseFloat(oldNum) - parseFloat(num));
+    } else if (operator === "+") {
+      setNum(parseFloat(oldNum) + parseFloat(num));
     }
-  }, [display]);
+  };
+
   return (
     <div className="calculator">
-      <div className="display">
-        <span>{display}</span>
-        <span>{result}</span>
-      </div>
-      <div className="keyboard">
-        <div className="top-row">
-          <button onClick={clearDisplay}>AC</button>
-          <button onClick={() => handleInput('-')}>-</button>
-          <button onClick={() => handleInput('+/-')}>±</button>
-          <button onClick={calculateResult}>=</button>
+      <Space>
+        <div className="wrapper">
+          <h1 className="resultado">{num}</h1>
+          <button className="grey-light" onClick={clear}>
+            C
+          </button>
+          <button className="grey-light" onClick={changeSign}>
+            +/-
+          </button>
+          <button className="grey-light" onClick={porcentagem}>
+            %
+          </button>
+          <button className="orange" onClick={operatorHandler} value={"/"}>
+            ÷
+          </button>
+          <button className="grey-number" onClick={inputNum} value={7}>
+            7
+          </button>
+          <button className="grey-number" onClick={inputNum} value={8}>
+            8
+          </button>
+          <button className="grey-number" onClick={inputNum} value={9}>
+            9
+          </button>
+          <button className="orange" onClick={operatorHandler} value={"X"}>
+            x
+          </button>
+          <button className="grey-number" onClick={inputNum} value={4}>
+            4
+          </button>
+          <button className="grey-number" onClick={inputNum} value={5}>
+            5
+          </button>
+          <button className="grey-number" onClick={inputNum} value={6}>
+            6
+          </button>
+          <button className="orange" onClick={operatorHandler} value={"-"}>
+            -
+          </button>
+          <button className="grey-number" onClick={inputNum} value={1}>
+            1
+          </button>
+          <button className="grey-number" onClick={inputNum} value={2}>
+            2
+          </button>
+          <button className="grey-number" onClick={inputNum} value={3}>
+            3
+          </button>
+          <button className="orange" onClick={operatorHandler} value={"+"}>
+            +
+          </button>
+          <button className="grey-zero" onClick={inputNum} value={0}>
+            0
+          </button>
+          <button className="grey-number" onClick={inputNum} value={"."}>
+            ,
+          </button>
+          <button className="orange" onClick={calculate}>
+            =
+          </button>
         </div>
-        <div className="bottom-row">
-          <button onClick={() => handleInput(7)}>7</button>
-          <button onClick={() => handleInput(8)}>8</button>
-          <button onClick={() => handleInput(9)}>9</button>
-          <button onClick={() => handleInput('/')}>÷</button>
-        </div>
-        <div className="middle-row">
-          <button onClick={() => handleInput(4)}>4</button>
-          <button onClick={() => handleInput(5)}>5</button>
-          <button onClick={() => handleInput(6)}>6</button>
-          <button onClick={() => handleInput('*')}>×</button>
-        </div>
-        <div className="bottom-row">
-          <button onClick={() => handleInput(1)}>1</button>
-          <button onClick={() => handleInput(2)}>2</button>
-          <button onClick={() => handleInput(3)}>3</button>
-          <button onClick={() => handleInput('%')}>%</button>
-        </div>
-      </div>
+      </Space>
     </div>
   );
 };
